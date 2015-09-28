ee93a23049f9bdfc6308510e8f66ffcbe390f491
Yerko Palma <yerko.palma@usach.cl>
2015-09-28 18:29:35 +0000

    fix(final): javascript issues

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -2,4 +2,5 @@ $(document).on('ready page:change', function() {
     Waves.displayEffect();
     $(".button-collapse").sideNav();
     $('.modal-trigger').leanModal();
+    $('.tooltipped').tooltip({delay: 50});
   });
