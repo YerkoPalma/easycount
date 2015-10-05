05bf3c6b4c45f4a94a6e6f8b7124e8ba9daa3e5a
YerkoPalma <yerko.palma@usach.cl>
2015-10-05 00:27:19 -0300

    add: first integration test for companies

---
+++ test/integration/company_creation_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class CompanyCreationTest < ActionDispatch::IntegrationTest
+
+  def setup
+    User.delete_all
+  end
+
+  test "invalid company creation" do
+
+  end
+end
